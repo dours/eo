feec3d16bcf1c00fdd4014a9aba0270eed7f6bd8
dours <akadours2@gmail.com>
2021-11-05 00:23:52 +0200

    add a magic printer, which does dataization

--- eo-runtime/src/main/java/EOorg/EOeolang/EOdebugMagic.java
+++ eo-runtime/src/main/java/EOorg/EOeolang/EOdebugMagic.java
@@ -9,7 +9,8 @@ public final class EOdebugMagic extends PhDefault {
 
     public EOdebugMagic(final Phi sigma) {
       super(sigma);
-      this.add("print", new AtOnce(new AtComposite(this, EOdebugMagic$EOprint::new)));
+        this.add("print", new AtOnce(new AtComposite(this, EOdebugMagic$EOprint::new)));
+        this.add("printDataized", new AtOnce(new AtComposite(this, EOdebugMagic$EOprintDataized::new)));
     }
 
     public final class EOdebugMagic$EOprint extends PhDefault {
@@ -26,4 +27,46 @@ public final class EOdebugMagic extends PhDefault {
         })));
       }
     }
+
+    public final class EOdebugMagic$EOprintDataized extends PhDefault {
+        public EOdebugMagic$EOprintDataized(final Phi sigma) {
+            super(sigma);
+            this.add(PREFIX, new AtFree(/* default */));
+            this.add(OBJ, new AtFree(/* default */));
+            this.add("φ", new AtOnce(new AtComposite(this, rho -> {
+                final String prefix = new Dataized(
+                        rho.attr(PREFIX).get()
+                ).take(String.class);
+                System.out.println(prefix +
+                        (new Dataized(rho.attr(OBJ).get()).take()).toString());
+                return new PhMethod(rho, "obj");
+            })));
+        }
+    }
+
+    public class EOdebugMagic$EOseq extends PhDefault {
+
+        /**
+         * Attribute name.
+         */
+        private static final String NAME = "steps";
+
+        public EOdebugMagic$EOseq(final Phi sigma) {
+            super(sigma);
+            this.add(NAME, new AtVararg());
+            this.add("φ", new AtComposite(this, self -> {
+                final Phi[] args = new Dataized(
+                        self.attr(NAME).get()
+                ).take(Phi[].class);
+                Object result = false;
+                for (final Phi arg : args) {
+                    result = new Dataized(arg).take();
+                }
+                return new Data.ToPhi(result);
+            }));
+        }
+
+    }
+
+
 }
