81d832e1dae2e99470d0f94da19e3428838aff71
dours <akadours2@gmail.com>
2021-11-05 01:59:47 +0200

    add seq magic, which can be used to insert any printer into any unrelated expression

---
+++ eo-runtime/src/main/java/EOorg/EOeolang/EOdebugMagic$EOseq.java
@@ -0,0 +1,16 @@
+package EOorg.EOeolang;
+
+import org.eolang.*;
+
+public class EOdebugMagic$EOseq extends PhDefault {
+    public EOdebugMagic$EOseq(final Phi sigma) {
+        super(sigma);
+        this.add("a", new AtFree());
+        this.add("b", new AtFree());
+        this.add("φ", new AtComposite(this, self -> {
+            new Dataized(self.attr("a").get()).take();
+            return new PhMethod(self, "b");
+        }));
+    }
+
+}
